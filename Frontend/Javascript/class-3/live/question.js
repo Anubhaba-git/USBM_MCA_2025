65fffca444e11e001f4c1ccdbc69d1f8b52a210a
Anubhaba Sahani <anubhabasahani87@gmail.com>
2024-10-08 23:03:31 +0530

    Day-3 task

---
+++ Frontend/Javascript/class-3/live/question.js
@@ -0,0 +1,70 @@
+// Q-1: Input no and know the daysName , using switch case.
+let dayNumber = 5;
+switch (dayNumber){
+     case 0:
+        console.log(`It's Monday`);
+        break;
+     case 1:
+        console.log(`It's Tuesday`);
+        break;
+     case 2:
+        console.log(`It's Wednesday`);
+        break;
+     case 3:
+        console.log(`It's Thursday`);
+        break;
+     case 4:
+        console.log(`It's Friday`);
+        break;
+     case 5:
+        console.log(`It's Saturday`);
+        break;
+    case 6:
+        console.log(`It's Sunday`);
+        break;
+    default:
+        console.log(`Invalid dayNumber !! Enter between 0-6`);
+}
+
+// Task-2:Input no and know the MonthsName , using switch case.
+let monthNumber = 5;
+switch (monthNumber){
+     case 0:
+        console.log(`It's January`);
+        break;
+     case 1:
+        console.log(`It's February`);
+        break;
+     case 2:
+        console.log(`It's March`);
+        break;
+     case 3:
+        console.log(`It's April`);
+        break;
+     case 4:
+        console.log(`It's May`);
+        break;
+     case 5:
+        console.log(`It's June`);
+        break;
+    case 6:
+        console.log(`It's July`);
+        break;
+    case 7:
+        console.log(`It's August`);
+        break;
+    case 8:
+        console.log(`It's September`);
+        break;
+    case 9:
+        console.log(`It's October`);
+        break;
+    case 10:
+        console.log(`It's November`);
+        break;
+    case 11:
+        console.log(`It's December`);
+        break;
+    default:
+        console.log(`Invalid monthNumber !! Enter between 0-11`);
+}
